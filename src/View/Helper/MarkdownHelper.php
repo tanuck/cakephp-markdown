57b40076dd619f5471871ba6ef2c8e87b2d0c458
James Tancock <tanuck@gmail.com>
2015-02-15 19:25:36 +0000

    Transform action

--- src/View/Helper/MarkdownHelper.php
+++ src/View/Helper/MarkdownHelper.php
@@ -2,6 +2,7 @@
 namespace Tanuck\Markdown\View\Helper;
 
 use Cake\View\Helper;
+use cebe\markdown\GithubMarkdown;
 
 /**
  * Markdown Helper
@@ -11,4 +12,18 @@ use Cake\View\Helper;
 class MarkdownHelper extends Helper
 {
 
+    /**
+     * Parse Markdown input to HTML 5.
+     *
+     * @param string $input Markdown to be parsed.
+     * @return string
+     */
+    public function transform($input)
+    {
+        if (!isset($this->parser)) {
+            $this->parser = new GithubMarkdown();
+            $this->parser->html5 = true;
+        }
+        return $this->parser->parse($input);
+    }
 }
