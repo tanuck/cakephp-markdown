ab1826a4c8a9e35ce9a45cb32008ef21c5509a3d
James Tancock <tanuck@gmail.com>
2015-02-18 16:52:22 +0000

    Fixed travis config and testing bootstrap

---
+++ tests/TestCase/View/Helper/MarkdownHelperTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Tanuck\Markdown\Test\TestCase\View\Helper;
+
+use Cake\TestSuite\TestCase;
+use Tanuck\Markdown\View\Helper\MarkdownHelper;
+
+/**
+ * Tanuck\Markdown\View\Helper\MarkdownHelper Test Case
+ */
+class MarkdownHelperTest extends TestCase
+{
+
+    public function setUp()
+    {
+    }
+
+    public function tearDown()
+    {
+    }
+
+    public function testTransform()
+    {
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,24 +1,87 @@
 <?php
-/**
- * Test suite bootstrap for Cakephp-markdown.
- *
- * This function is used to find the location of CakePHP whether CakePHP
- * has been installed as a dependency of the plugin, or the plugin is itself
- * installed as a dependency of an application.
- */
-$findRoot = function ($root) {
-    do {
-        $lastRoot = $root;
-        $root = dirname($root);
-        if (is_dir($root . '/vendor/cakephp/cakephp')) {
-            return $root;
-        }
-    } while ($root !== $lastRoot);
+use Cake\Cache\Cache;
+use Cake\Core\Configure;
+use Cake\Core\Plugin;
+use Cake\Datasource\ConnectionManager;
+use Cake\Log\Log;
 
-    throw new Exception("Cannot find the root of the application, unable to run tests");
-};
-$root = $findRoot(__FILE__);
-unset($findRoot);
+// Path constants to a few helpful things.
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__DIR__) . DS);
+define('CAKE_CORE_INCLUDE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
+define('CORE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS);
+define('CAKE', CORE_PATH . 'src' . DS);
+define('APP', ROOT . 'tests' . DS . 'test_app' . DS);
+define('TMP', sys_get_temp_dir() . DS);
+define('CACHE', TMP);
+define('LOGS', TMP);
 
-chdir($root);
-require $root . '/config/bootstrap.php';
+require_once 'vendor/autoload.php';
+require_once CORE_PATH . 'config/bootstrap.php';
+
+date_default_timezone_set('UTC');
+mb_internal_encoding('UTF-8');
+
+Configure::write('debug', true);
+Configure::write('App', [
+    'namespace' => 'App',
+    'encoding' => 'UTF-8',
+    'base' => false,
+    'baseUrl' => false,
+    'dir' => 'src',
+    'webroot' => 'webroot',
+    'www_root' => APP . 'webroot',
+    'fullBaseUrl' => 'http://localhost',
+    'imageBaseUrl' => 'img/',
+    'jsBaseUrl' => 'js/',
+    'cssBaseUrl' => 'css/',
+    'paths' => [
+        'plugins' => [APP . 'Plugin' . DS],
+        'templates' => [APP . 'Template' . DS]
+    ]
+]);
+Configure::write('Session', [
+    'defaults' => 'php'
+]);
+Cache::config([
+    '_cake_core_' => [
+        'engine' => 'File',
+        'prefix' => 'cake_core_',
+        'serialize' => true
+    ],
+    '_cake_model_' => [
+        'engine' => 'File',
+        'prefix' => 'cake_model_',
+        'serialize' => true
+    ],
+    'default' => [
+        'engine' => 'File',
+        'prefix' => 'default_',
+        'serialize' => true
+    ]
+]);
+
+ConnectionManager::config('test', [
+    'className' => 'Cake\Database\Connection',
+    'driver' => 'Cake\Database\Driver\Mysql',
+    'host' => 'localhost',
+    'database' => 'markdown_test',
+    'username' => 'travis',
+    'password' => '',
+    'timezone' => 'UTC'
+]);
+
+Log::config([
+    'debug' => [
+        'engine' => 'Cake\Log\Engine\FileLog',
+        'levels' => ['notice', 'info', 'debug'],
+        'file' => 'debug',
+    ],
+    'error' => [
+        'engine' => 'Cake\Log\Engine\FileLog',
+        'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
+        'file' => 'error',
+    ]
+]);
+
+Plugin::load('Tanuck/Markdown', ['path' => ROOT]);
